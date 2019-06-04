a82aeb710be576d6070981fccc65b1657cde12b6
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-04 23:28:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  
+  i = 1 
+  while i < array.length 
+    yield(array[i])
+    i += 1
+    my_each(array) do |i|
+      puts i 
+  end
+    array
 end
